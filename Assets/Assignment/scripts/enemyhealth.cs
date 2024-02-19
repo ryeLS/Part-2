7741bd40fe8b9e8641128b6cab3f56a77af3256c
luesue <luesue@OA-S143-04>
2024-02-18 20:59:38 -0500

    will continue later

---
+++ Assets/Assignment/scripts/enemyhealth.cs
@@ -0,0 +1,24 @@
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//https://opengameart.org/content/flying-dragon-rework
+//https://opengameart.org/content/simple-health-bars
+public class enemyhealth : MonoBehaviour
+{
+    public float health;
+    public float currentHealth;
+    public Slider slider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
